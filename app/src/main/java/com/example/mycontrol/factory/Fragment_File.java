c63e7e741d78c90349a6de404fbba11237ee8986
Mazzotta-Fabio <mazzottafabio94@gmail.com>
2025-01-24 14:40:33 +0100

    aggiornamento definitivo fragment file

--- app/src/main/java/com/example/mycontrol/factory/Fragment_File.java
+++ app/src/main/java/com/example/mycontrol/factory/Fragment_File.java
@@ -92,6 +92,7 @@ public class Fragment_File extends Fragment implements View.OnClickListener {
     @Override
     public void onClick(View v) {
         if (v.getId() == R.id.CaricaFile) {
+
             chooseItem="";
             //imposta alert
             AlertDialog.Builder ac = new AlertDialog.Builder(v.getContext());
@@ -165,10 +166,10 @@ public class Fragment_File extends Fragment implements View.OnClickListener {
                     }
                 }
             }
-            arrayStream.clear();
-            nomiFile.clear();
-            txtNomeFile.setText("Nessun file selezionato");
         }
+        arrayStream.clear();
+        nomiFile.clear();
+        txtNomeFile.setText("Nessun file caricato");
     }
 
     public void partiIntent(String chooseItem) {
